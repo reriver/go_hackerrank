831fef19ae90909373b425b7d03e7b038e3137b0
Jorge <71904707+reriver@users.noreply.github.com>
2021-08-01 18:47:06 +0300

    12 pick 3

--- 8pickingNumbers.go
+++ 8pickingNumbers.go
@@ -6,75 +6,48 @@ import (
 )
 
 func pickingNumbers(a []int32) int32 {
+
+	if len(a) < 2 || len(a) > 100 {
+		fmt.Errorf("2 <= n <= 100")
+		return int32(0)
+	}
+
 	fmt.Println(a)
 	sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
 	fmt.Println(a)
 
-	var maxCount int32 = -1
-	var count int32 = 0
-	for i := 0; i < len(a)-1; i++ {
-		if 0 <= a[i+1]-a[i] && a[i+1]-a[i] < 2 {
-			count++
-			if count > maxCount {
-				maxCount = count
-			}
-		} else {
-			count = 0
-		}
-		if a[i+1]-a[i] < 0 {
-			fmt.Errorf("erro array not sorted")
-			return -1
-		}
-		fmt.Println("MaxCount = ", maxCount)
-		fmt.Println("count = ", count)
+	var count = [100]int{}
+
+	for i := 0; i < len(a); i++ {
+		count[a[i]]++
 	}
-	return maxCount
+	fmt.Println(count)
+
+	var neighbour = []int{}
+	for i := 0; i < len(count)-1; i++ {
+		//if count[i] > 0 && count[i+1] > 0 {
+		neighbour = append(neighbour, count[i]+count[i+1])
+		//}
+	}
+	fmt.Println("neighbour ", neighbour)
+	sort.Ints(neighbour)
+	fmt.Println("neighbour", neighbour, "sorted")
+
+	return int32(neighbour[len(neighbour)-1])
 }
 
 func main() {
-	//reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
-	//
-	//stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	//checkError(err)
-	//
-	//defer stdout.Close()
-	//
-	//writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
-	//
-	//nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	//checkError(err)
-	//n := int32(nTemp)
-
-	//aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
 
 	var a = []int32{4, 6, 5, 3, 3, 1}
-
-	//for i := 0; i < int(n); i++ {
-	//	aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
-	//	checkError(err)
-	//	aItem := int32(aItemTemp)
-	//	a = append(a, aItem)
+	//var a = []int32{1, 2, 2, 3, 1, 2}
+	//var a []int32
+	//
+	//for i := 0; i < 100; i++ {
+	//	a = append(a, 66)
 	//}
 
 	result := pickingNumbers(a)
 
 	fmt.Printf("%d\n", result)
 
-	//writer.Flush()
 }
-
-//
-//func readLine(reader *bufio.Reader) string {
-//	str, _, err := reader.ReadLine()
-//	if err == io.EOF {
-//		return ""
-//	}
-//
-//	return strings.TrimRight(string(str), "\r\n")
-//}
-//
-//func checkError(err error) {
-//	if err != nil {
-//		panic(err)
-//	}
-//}
